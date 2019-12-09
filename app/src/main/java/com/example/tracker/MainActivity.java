f027e5fdaabb946b9f9558df40aaa9ca4cc0f029
Sandhya <msandya8@gmail.com>
2019-12-09 13:31:08 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/tracker/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+    }
+}
